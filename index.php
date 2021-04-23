08fa729ad718a541ebad1f8635bda5cad9af8e92
Jean-Kenneth Antonio <jantonio2@mail.greenriver.edu>
2021-04-23 18:43:07 -0400

    Addes comments and fix code spacing

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
-
-//This is my controller
+/*
+ * Jean-Kenneth Antonio
+ * April 23, 2021
+ * 328/dating/views/home.html
+ *
+ * This is my controller for dating website
+ * */
 
 //Turn on error reporting
 ini_set('display_errors', 1);
@@ -17,7 +22,7 @@ $f3 -> route('GET /', function() {
     //display the homepage
     $view = new Template();
     echo $view->render('views/home.html');
-}
+    }
 );
 
 //Run fat free
